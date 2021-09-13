3050a40d51df3184486ed14fb838533f810b3bbb
0xSzeth <0xSzeth@gmail.com>
2021-09-13 10:14:04 -0600

    Update the DInterest src file

--- src/DInterest.ts
+++ src/DInterest.ts
@@ -1,45 +1,124 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { DInterest, Upgraded } from "../generated/DInterest/DInterest"
-import { ExampleEntity } from "../generated/schema"
+import { BigDecimal, BigInt, Address, ByteArray, crypto, ethereum } from "@graphprotocol/graph-ts";
+import {
+  DInterest,
+  EDeposit,
+  EFund,
+  EPayFundingInterest,
+  ERolloverDeposit,
+  ESetParamAddress,
+  ESetParamUint,
+  ETopupDeposit,
+  EWithdraw,
+  OwnershipTransferred
+} from "../generated/cDAIPool/DInterest";
+import { IInterestOracle } from "../generated/cDAIPool/IInterestOracle";
+import { DPool } from "../generated/schema";
 
-export function handleUpgraded(event: Upgraded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+let YEAR = BigInt.fromI32(31556952); // One year in seconds
+let ZERO_DEC = BigDecimal.fromString('0');
+let ONE_DEC = BigDecimal.fromString('1');
+let NEGONE_DEC = BigDecimal.fromString('-1');
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+let POOL_ADDRESSES = new Array<string>(0);
+POOL_ADDRESSES.push("0x11B1c87983F881B3686F8b1171628357FAA30038"); // cDAI
+// POOL_ADDRESSES.push("0x3d59EcA28fC3CA2338951A7C8E0C435a1691550b"); // cUSDC
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+let POOL_STABLECOIN_DECIMALS = new Array<i32>(0);
+POOL_STABLECOIN_DECIMALS.push(18); // cDAI
+// POOL_STABLECOIN_DECIMALS.push(6); // cUSDC
+
+let POOL_DEPLOY_BLOCKS = new Array<i32>(0);
+POOL_DEPLOY_BLOCKS.push(13135260) // cDAI
+// POOL_DEPLOY_BLOCKS.push(9146306) // cUSDC
+
+export function keccak256(s: string): ByteArray {
+  return crypto.keccak256(ByteArray.fromUTF8(s));
+}
+
+export function tenPow(exponent: number): BigInt {
+  let result = BigInt.fromI32(1);
+  for (let i = 0; i < exponent; i++) {
+    result = result.times(BigInt.fromI32(10));
   }
+  return result;
+}
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+export function normalize(i: BigInt, decimals: number = 18): BigDecimal {
+  return i.toBigDecimal().div(new BigDecimal(tenPow(decimals)));
+}
 
-  // Entity fields can be set based on event parameters
-  entity.implementation = event.params.implementation
+export function getPool(poolAddress: string): DPool {
+  let pool = DPool.load(poolAddress);
+  if (pool == null) {
+    pool = new DPool(poolAddress);
+    let poolContract = DInterest.bind(Address.fromString(poolAddress));
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+    pool.address = poolAddress;
+    pool.moneyMarket = poolContract.moneyMarket().toHex();
+    pool.stablecoin = poolContract.stablecoin().toHex();
+    pool.interestModel = poolContract.interestModel().toHex();
+    pool.oneYearInterestRate = ZERO_DEC;
+    pool.oracleInterestRate = ZERO_DEC;
+    pool.surplus = ZERO_DEC;
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+    pool.save();
+  }
+  return pool as DPool;
+}
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.admin(...)
-  // - contract.implementation(...)
+export function handleEDeposit(event: EDeposit): void {}
+
+export function handleEFund(event: EFund): void {}
+
+export function handleEPayFundingInterest(event: EPayFundingInterest): void {}
+
+export function handleERolloverDeposit(event: ERolloverDeposit): void {}
+
+export function handleESetParamAddress(event: ESetParamAddress): void {
+  let pool = getPool(event.address.toHex());
+  let paramName = event.params.paramName;
+  if (paramName == keccak256("interestModel")) {
+    pool.interestModel = event.params.newValue.toHex();
+  }
+  pool.save();
+}
+
+export function handleESetParamUint(event: ESetParamUint): void {}
+
+export function handleETopupDeposit(event: ETopupDeposit): void {}
+
+export function handleEWithdraw(event: EWithdraw): void {}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handleBlock(block: ethereum.Block): void {
+  let blockNumber = block.number.toI32();
+  for (let i = 0; i < POOL_ADDRESSES.length; i++) {
+    if (blockNumber >= POOL_DEPLOY_BLOCKS[i]) {
+
+      let poolID = POOL_ADDRESSES[i];
+      let pool = getPool(poolID);
+      let poolContract = DInterest.bind(Address.fromString(pool.address));
+      let stablecoinDecimals: number = POOL_STABLECOIN_DECIMALS[i];
+      let oracleContract = IInterestOracle.bind(poolContract.interestOracle());
+
+      let oneYearInterestRate = poolContract.try_calculateInterestAmount(tenPow(18), YEAR);
+      pool.oneYearInterestRate = oneYearInterestRate.reverted
+        ? null
+        : normalize(oneYearInterestRate.value)
+
+      let oracleInterestRate = oracleContract.try_updateAndQuery();
+      pool.oracleInterestRate = oracleInterestRate.reverted
+        ? null
+        : normalize(oracleInterestRate.value.value1)
+
+      let surplusResult = poolContract.try_surplus();
+      pool.surplus = surplusResult.reverted
+        ? null
+        : normalize(surplusResult.value.value1, stablecoinDecimals).times(surplusResult.value.value0 ? NEGONE_DEC : ONE_DEC)
+
+      pool.save();
+
+    }
+  }
 }
